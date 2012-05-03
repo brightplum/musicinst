017479f29a775ec3ead429cdeebf940fe789a5a5
Fredric Mitchell <fmitchell@phase2technology.com>
2012-05-03 10:46:32 -0500

    fix left navigation

--- themes/mic/template.php
+++ themes/mic/template.php
@@ -8,7 +8,7 @@ function mic_preprocess_page(&$vars) {
 	if(!empty($vars['page']['left_navigation'])) {
 
 		$links = mic_build_left_navigation($vars['page']['left_navigation']);
-		
+
 		if(!empty($links)) {
 			$vars['left_navigation'] = '<ul class="menu">';
 			foreach($links as $link) {
@@ -26,22 +26,22 @@ function mic_preprocess_page(&$vars) {
  * Template override for theme_breadcrumb().
  */
 function mic_breadcrumb($breadcrumb) {
-	
+
 	if (!empty($breadcrumb)) {
-	
+
 		$lastitem = sizeof($breadcrumb['breadcrumb']);
 		$crumbs = '<ul>';
 		$a=1;
 
 		foreach ($breadcrumb['breadcrumb'] as $value) {
-			
+
 			if ($a == 1) {
-				
+
 				if ($a == sizeof($breadcrumb['breadcrumb']))
 					$crumbs .= '<li class="first last">'.$value.'</li>';
 				else
 					$crumbs .= '<li class="first">'.$value.'</li>';
-				
+
 				$a++;
 
 			} elseif ($a == $lastitem) {
@@ -51,16 +51,16 @@ function mic_breadcrumb($breadcrumb) {
 			} else {
 
 				$crumbs .= '<li>'.$value.'</li>';
-				$a++;	
+				$a++;
 
-			}	
+			}
 
 		}
 
 		$crumbs .= '</ul>';
 
 	}
-	
+
 	return $crumbs;
 
 }
@@ -75,9 +75,9 @@ function mic_get_display_body($body, $trim_length = 200) {
 
 
 function mic_build_left_navigation($nav) {
-	
+
 	$links = array();
-	
+
 	if(isset($nav['menu_block_1'])) {
 		foreach($nav['menu_block_1']['#content'] as $key => $item) {
 			if(is_integer($key)) {
@@ -86,70 +86,70 @@ function mic_build_left_navigation($nav) {
 			}
 		}
 	}
-	
+
 	if(isset($nav['menu_block_2'])) {
 		foreach($nav['menu_block_2']['#content'] as $key => $item) {
 			if(is_integer($key)) {
 				$link = array('element' => $item);
 				$links[] = theme('menu_link', $link);
 			}
-		}	
+		}
 	}
-	
+
 	if(isset($nav['menu_block_3'])) {
 		foreach($nav['menu_block_3']['#content'] as $key => $item) {
 			if(is_integer($key)) {
 				$link = array('element' => $item);
 				$links[] = theme('menu_link', $link);
 			}
-		}	
+		}
 	}
-	
+
 	if(isset($nav['menu_block_4'])) {
 		foreach($nav['menu_block_4']['#content'] as $key => $item) {
 			if(is_integer($key)) {
 				$link = array('element' => $item);
 				$links[] = theme('menu_link', $link);
 			}
-		}	
+		}
 	}
-	
+
 	if(isset($nav['menu_block_5'])) {
 		foreach($nav['menu_block_5']['#content'] as $key => $item) {
 			if(is_integer($key)) {
 				$link = array('element' => $item);
 				$links[] = theme('menu_link', $link);
 			}
-		}	
+		}
 	}
-	
+
 	if(isset($nav['menu_block_6'])) {
 		foreach($nav['menu_block_6']['#content'] as $key => $item) {
 			if(is_integer($key)) {
 				$link = array('element' => $item);
 				$links[] = theme('menu_link', $link);
 			}
-		}	
+		}
 	}
-	
+
 	if(isset($nav['menu_block_7'])) {
 		foreach($nav['menu_block_7']['#content'] as $key => $item) {
 			if(is_integer($key)) {
 				$link = array('element' => $item);
 				$links[] = theme('menu_link', $link);
 			}
-		}	
+		}
 	}
-	
+
 	if(isset($nav['menu_block_8'])) {
 		foreach($nav['menu_block_8']['#content'] as $key => $item) {
 			if(is_integer($key)) {
 				$link = array('element' => $item);
 				$links[] = theme('menu_link', $link);
 			}
-		}	
+		}
 	}
-	
+
 	return $links;
 }
 
@@ -162,34 +162,34 @@ function mic_preprocess_calendar_day(&$vars) {
 	$rows = $vars['rows'];
 	$start_times = array_keys($rows['items']);
 	foreach ($start_times as $start_time) {
-		
+
 		$next_start_time = array_key_exists($i + 1, $start_times) ? $start_times[$i + 1] : '23:59:59';
 		$variables = array(
-			'start_time' => $start_time, 
-			'next_start_time' => $next_start_time, 
+			'start_time' => $start_time,
+			'next_start_time' => $next_start_time,
 			'curday_date' => $rows['date'],
 		);
 		$heading = mic_calendar_time_row_heading($variables);
-		
+
 		$grouped_items[$start_time]['hour'] = $heading['hour'];
 		$grouped_items[$start_time]['ampm'] = $heading['ampm'];
 		$grouped_items[$start_time]['values']  = $rows['items'][$start_time]['values'];
-		$i++;      
+		$i++;
 	}
-	
+
 	if(isset($grouped_items)) {
 		ksort($grouped_items);
 	  	$vars['rows']['items'] = $grouped_items;
 	}
-  	
+
 }
 
 function mic_calendar_time_row_heading($vars) {
-	
+
 	$start_time = $vars['start_time'];
 	$next_start_time = $vars['next_start_time'];
 	$curday_date = $vars['curday_date'];
-	
+
 	static $format_hour, $format_ampm;
 	if (empty($format_hour)) {
 	    $format = variable_get('date_format_short', 'm/d/Y - H:i');
