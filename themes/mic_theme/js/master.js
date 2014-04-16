214561ce654a10f8d40cce6a3b9817fc0ac9e2a5
fmitchell <fmitchell@phase2technology.com>
2014-04-16 09:47:12 -0500

    mask for input

--- themes/mic_theme/js/master.js
+++ themes/mic_theme/js/master.js
@@ -4,6 +4,12 @@ Drupal.behaviors.mic_mask = {
   attach: function (context, settings) {
     $(".form-phone", context).mask("(999) 999-9999");
     $(".form-date", context).mask("99/99/9999");
+    $("#edit-line-item-fields-field-student-birthdate-und-0-value", context).mask("99/99/9999");
+    $("#edit-line-item-fields-field-student-birthdate-und-0-value--2", context).mask("99/99/9999");
+    $("#edit-line-item-fields-field-student-birthdate-und-0-value--3", context).mask("99/99/9999");
+    $("#edit-line-item-fields-field-requested-time-und-0-value", context).mask("99:99");
+    $("#edit-line-item-fields-field-requested-time-und-0-value--2", context).mask("99:99");
+    $("#edit-line-item-fields-field-requested-time-und-0-value--3", context).mask("99:99");
   }
 };
 
