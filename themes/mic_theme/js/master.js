49b17f9958fd9b8509e07f2eeee866700f4cba0b
fmitchell <fmitchell@phase2technology.com>
2014-05-04 23:13:13 -0500

    instrument option; installment plan pieces; credit fee check; part-time, full-time

--- themes/mic_theme/js/master.js
+++ themes/mic_theme/js/master.js
@@ -4,6 +4,8 @@ Drupal.behaviors.mic_mask = {
   attach: function (context, settings) {
     $(".form-phone", context).mask("(999) 999-9999");
     $(".form-date", context).mask("99/99/9999");
+    $("#edit-birthdate", context).mask("99/99/9999");
+    $("#edit-time", context).mask("99:99 aa");
   }
 };
 
